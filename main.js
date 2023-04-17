a7d857355493d5bab8c6d8ce8762d767dd2155f0
kenanakperov <kenanakperov5gmail.com>
2023-04-17 22:43:56 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// ! Task 1 ============================================================
+
+// let a = parseInt(prompt("Reqemi daxil edin: "))
+
+
+// function numberType(a){
+//     if(a % 2 == 0){
+//         alert("Its even")
+//     }else{
+//         alert("Its odd")
+//     }
+// }
+// numberType(a)
+
+
+// ! Task 2,3 ============================================================
+
+
+
+// let login = prompt("Adı daxil edin: ")
+// let password = prompt("Sifreni daxil edin: ")
+
+// let userLogin = "ziyad"
+// let userPassword = "ziyad123"
+
+// let adminLogin = "kenan"
+// let adminPassword = "kenan123"
+
+// class loginPage{
+//     constructor(login,password,userLogin,userPassword,adminLogin,adminPassword){
+//         this.login = login;
+//         this.password=password;
+//         this.userLogin=userLogin;
+//         this.userPassword=userPassword;
+//         this.adminLogin=adminLogin
+//         this.adminPassword=adminPassword
+//     }
+//     switchUser () {
+//         if(login==userLogin && password==userPassword){
+//             alert("Welcome user: ")
+//         }else if(login==adminLogin && password==adminPassword){
+//             alert("Welcome admin: ")
+//         }else{
+//             alert("Login ve ya parol yanlisdir")
+//         }
+//     }
+// }
+
+// let newUser = new loginPage(login,password,userLogin,userPassword,adminLogin,adminPassword)
+
+// newUser.switchUser()
